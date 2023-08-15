4e09fbb3d1dbddebda05a775e1a1f08f73b1e9bd
varundhand <varundhand77@gmail.com>
2023-08-15 22:16:44 +0530

    Add: Add Navbar component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import Layout from "@/components/Layout";
 import "@/styles/globals.css"; // we cant import global stylesheets directly into the components
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  ); // page components
 }
