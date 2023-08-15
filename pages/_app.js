4928d4225d6a6b1baf3891bbd43ff0a5a4beec03
varundhand <varundhand77@gmail.com>
2023-08-15 16:59:54 +0530

    New: Add  About Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css"; // we cant import global stylesheets directly into the components
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
